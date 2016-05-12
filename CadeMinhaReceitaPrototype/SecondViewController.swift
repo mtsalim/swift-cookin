1167edb41fe931975dd8f7d877fa9a654afbf64c
brunospontes <bpontes@inf.puc-rio.br>
2016-05-12 11:03:39 -0300

    Initial Commit

---
+++ CadeMinhaReceitaPrototype/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  CadeMinhaReceitaPrototype
+//
+//  Created by Student on 5/12/16.
+//  Copyright © 2016 olhaMasQueGarotoMalCriado. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
